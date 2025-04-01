41cef7aec1d5bc920e0b6489698bf12541851967
Rosa_Sot12 <18390846@uagro.mx>
2025-04-01 11:52:01 -0600

    subiendo ejercicio

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function completarDatos (){
+    const name = prompt('¿Cuál es tu nombre?: ')
+    const age = prompt('¿Cúal es tu año de nacimiento?: ')
+    const city = prompt('¿En que ciudad vives?')
+    const js = prompt('¿Te interesa JavaScript? Sí/No')
+}
